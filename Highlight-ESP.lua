ac42443f0659c71418457f261a87fd41e0393106
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-02-10 19:40:49 +0800

    Create Highlight-ESP.lua

---
+++ Highlight-ESP.lua
@@ -0,0 +1,48 @@
+local holder = game.CoreGui:FindFirstChild("ESPHolder") or Instance.new("Folder")
+if enabled == false then
+    holder:Destroy()
+end
+
+if holder.Name == "Folder" then
+    holder.Name = "ESPHolder"
+    holder.Parent = game.CoreGui
+end
+
+if uselocalplayer == false and holder:FindFirstChild(game.Players.LocalPlayer.Name) then
+    holder:FindFirstChild(game.Players.LocalPlayer.Name):Destroy()
+end
+
+if getgenv().enabled == true then 
+    getgenv().enabled = false
+    getgenv().enabled = true
+end
+game:GetService("RunService").Heartbeat:Connect(function()
+    if getgenv().enabled then
+        task.wait()
+        for _,v in pairs(game.Players:GetChildren()) do
+            local chr = v.Character
+            if chr ~= nil then
+                local esp = holder:FindFirstChild(v.Name) or Instance.new("Highlight")
+                esp.Name = v.Name
+                if uselocalplayer == false and esp.Name == game.Players.LocalPlayer.Name then
+                    else
+                esp.Parent = holder
+                if filluseteamcolor then
+                    esp.FillColor = v.TeamColor.Color
+                else
+                    esp.FillColor = fillcolor 
+                end
+                if outlineuseteamcolor then
+                    esp.OutlineColor = v.TeamColor.Color
+                else
+                    esp.OutlineColor = outlinecolor    
+                end
+                esp.FillTransparency = filltrans
+                esp.OutlineTransparency = outlinetrans
+                esp.Adornee = chr
+                esp.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+            end
+        end
+    end
+end
+end)
