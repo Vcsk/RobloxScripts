4fc640adb517406fc24a4620742cd52443c4f3f5
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-02-18 08:00:25 +0800

    Update AimViewer.lua

--- DaHood/AimViewer.lua
+++ DaHood/AimViewer.lua
@@ -3,6 +3,6 @@ _G.color = Color3.fromRGB(255,0,0)
 _G.toggle_keybind = "l" -- enable tracer and disable
 _G.swith_nigga = 't' -- press t and u will see a noti on the user ur tracer is on 
 _G.method = "MousePos" --had a stroke sotkraakdakdakdkadkadkakdakdakdkdakdakdkadkadka
--- ANTI KEYBIND IS Z
+_G.antilockenabled = false -- true/false ANTI KEYBIND IS Z
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/DaHood/AimViewerSC.lua"))()
