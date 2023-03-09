ff3e5f80d8b7297cbafdfd3672757b432e5afe39
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-03-10 00:24:43 +0800

    Update Highlight-ESP.lua

--- Highlight-ESP.lua
+++ Highlight-ESP.lua
@@ -1,12 +1,12 @@
 --[[ Main
 
-getgenv().enabled = true --Toggle true/false
-getgenv().filluseteamcolor = true --Toggle fill color using player team color true/false
-getgenv().outlineuseteamcolor = true --Toggle outline color using player team color true/false
-getgenv().fillcolor = Color3.new(0, 0, 0) --Change fill color, no need to edit if using team color
-getgenv().outlinecolor = Color3.new(1, 1, 1) --Change outline color, no need to edit if using team color
-getgenv().filltrans = 0.5 --Change fill transparency
-getgenv().outlinetrans = 0.5 --Change outline transparency
+getgenv().enabled = true -- Toggle true/false
+getgenv().filluseteamcolor = true -- Toggle fill color using player team color true/false
+getgenv().outlineuseteamcolor = true -- Toggle outline color using player team color true/false
+getgenv().fillcolor = Color3.new(0, 0, 0) -- Change fill color, no need to edit if using team color
+getgenv().outlinecolor = Color3.new(1, 1, 1) -- Change outline color, no need to edit if using team color
+getgenv().filltrans = 0.5 -- Change fill transparency
+getgenv().outlinetrans = 0.5 -- Change outline transparency
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/Highlight-ESP.lua"))()
 
