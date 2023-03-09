30dd6f8c1016201be899b2c5f61c695e8b5e59a6
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-03-10 00:23:16 +0800

    Update Highlight-ESP.lua

--- Highlight-ESP.lua
+++ Highlight-ESP.lua
@@ -1,3 +1,17 @@
+--[[ Main
+
+getgenv().enabled = true --Toggle true/false
+getgenv().filluseteamcolor = true --Toggle fill color using player team color true/false
+getgenv().outlineuseteamcolor = true --Toggle outline color using player team color true/false
+getgenv().fillcolor = Color3.new(0, 0, 0) --Change fill color, no need to edit if using team color
+getgenv().outlinecolor = Color3.new(1, 1, 1) --Change outline color, no need to edit if using team color
+getgenv().filltrans = 0.5 --Change fill transparency
+getgenv().outlinetrans = 0.5 --Change outline transparency
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/Highlight-ESP.lua"))()
+
+]]
+
 local holder = game.CoreGui:FindFirstChild("ESPHolder") or Instance.new("Folder")
 if enabled == false then
     holder:Destroy()
