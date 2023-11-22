036ec9a75edbe7bb0e9b13201c8c9633be239b46
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-11-22 14:13:06 +0800

    Update HitboxExpander.lua

--- HitboxExpander.lua
+++ HitboxExpander.lua
@@ -208,3 +208,16 @@ getgenv().outlinetrans = 0.5 --Change outline transparency
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/Highlight-ESP.lua"))()
 end)
+
+if game.PlaceId == 3082002798 then
+    local GamesTab = Window:Tab("Games","rbxassetid://15426471035")
+	GamesTab:InfoLabel("Game: "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
+	GamesTab:Button("No Cooldown", function()
+	    for i, v in pairs(game:GetService('ReplicatedStorage')['Shared_Modules'].Tools:GetDescendants()) do
+		    if v:IsA('ModuleScript') then
+			    local Module = require(v)
+				Module.DEBOUNCE = 0
+			end
+		end
+	end)
+end
