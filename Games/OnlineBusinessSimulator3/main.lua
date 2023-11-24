321410b5ca88d9ef4dc62e771898f46c269bebc8
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-11-25 00:47:11 +0800

    Update main.lua

--- Games/OnlineBusinessSimulator3/main.lua
+++ Games/OnlineBusinessSimulator3/main.lua
@@ -1,1 +1,65 @@
+getgenv().settings = {
+    AutoWork = false,
+    AutoCollectWork = false,
+}
 
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/UI-Library/main/Source/Orion.lua"))()
+local window = OrionLib:MakeWindow({Name = "Online Business Simulator 3", HidePremium = false, SaveConfig = true, ConfigFolder = "VX_OBS3", IntroEnabled = true, IntroText = "Made by vcsk0"})
+
+local homeTab = window:MakeTab({
+    Name = "Home",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+local mainTab = window:MakeTab({
+    Name = "Main",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+homeTab:AddParagraph("Info","[BETA] - UPDATE 1: [Added Auto Work, Added Auto Collect Work]")
+
+homeTab:AddButton({
+    Name = "Discord Invite",
+    Callback = function()
+              setclipboard("https://discord.com/invite/cNBNvSufYw")
+              OrionLib:MakeNotification({
+                  Name = "Notification",
+                  Content = "Copied to your clipboard!",
+                  Image = "rbxassetid://4483345998",
+                  Time = 5
+              })
+      end
+})
+
+mainTab:AddToggle({
+    Name = "Auto Work",
+    Default = false,
+    Callback = function(toggle)
+        getgenv().settings.AutoWork = toggle
+    end
+})
+
+mainTab:AddToggle({
+    Name = "Auto Collect Work",
+    Default = false,
+    Callback = function(toggle)
+        getgenv().settings.AutoCollectWork = toggle
+    end
+})
+
+--// Main Codes
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if getgenv().settings.AutoWork == true then
+        game:GetService("ReplicatedStorage").Events.StartWorking:FireServer()
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if getgenv().settings.AutoCollectWork == true then
+        game:GetService("ReplicatedStorage").Events.CollectWork:InvokeServer()
+    end
+end)
+
+OrionLib:Init()
