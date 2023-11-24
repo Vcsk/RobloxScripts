0f6ebf71a28fa1471597877241203e9e046b759b
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-11-25 04:46:39 +0800

    Update main.lua

--- Games/OnlineBusinessSimulator3/main.lua
+++ Games/OnlineBusinessSimulator3/main.lua
@@ -2,6 +2,7 @@ getgenv().settings = {
     AutoTap = false,
 	AutoWork = false,
     AutoCollectWork = false,
+	AutoLoadSave = false,
 }
 
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/UI-Library/main/Source/Orion.lua"))()
@@ -17,8 +18,13 @@ local mainTab = window:MakeTab({
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
+local settingsTab = window:MakeTab({
+    Name = "Settings",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
 
-homeTab:AddParagraph("UPDATES","UPDATE 1: [Added Auto Work, Added Auto Collect Work]\nUPDATE 2: [Added Auto Tap]")
+homeTab:AddParagraph("UPDATES","UPDATE 1: Added Auto Work, Added Auto Collect Work\nUPDATE 2: Added Auto Tap")
 
 homeTab:AddButton({
     Name = "Discord Invite",
@@ -36,6 +42,8 @@ homeTab:AddButton({
 mainTab:AddToggle({
     Name = "Auto Tap",
     Default = false,
+	Save = true,
+	Flag = "AutoTap",
     Callback = function(toggle)
         getgenv().settings.AutoTap = toggle
     end
@@ -44,6 +52,8 @@ mainTab:AddToggle({
 mainTab:AddToggle({
     Name = "Auto Work",
     Default = false,
+	Save = true,
+	Flag = "AutoWork",
     Callback = function(toggle)
         getgenv().settings.AutoWork = toggle
     end
@@ -52,8 +62,20 @@ mainTab:AddToggle({
 mainTab:AddToggle({
     Name = "Auto Collect Work",
     Default = false,
+	Save = true,
+	Flag = "AutoCollectWork",
     Callback = function(toggle)
         getgenv().settings.AutoCollectWork = toggle
+    end
+})
+
+settingsTab:AddToggle({
+    Name = "Auto Load Saved Configs",
+    Default = false,
+	Save = true,
+	Flag = "AutoLoadSave",
+    Callback = function(toggle)
+        getgenv().settings.AutoLoadSave = toggle
     end
 })
 
@@ -73,6 +95,24 @@ end)
 
 game:GetService("RunService").Heartbeat:Connect(function()
     if getgenv().settings.AutoCollectWork == true then
+        game:GetService("ReplicatedStorage").Events.CollectWork:InvokeServer()
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if OrionLib.Flags["AutoLoadSave"].Value == true and OrionLib.Flags["AutoTap"].Value == true then
+        game:GetService("Players").LocalPlayer.PlayerGui.TapTapGameplay.TapServer.ServerEvent:FireServer()
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if OrionLib.Flags["AutoLoadSave"].Value == true and OrionLib.Flags["AutoWork"].Value == true then
+        game:GetService("ReplicatedStorage").Events.StartWorking:FireServer()
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if OrionLib.Flags["AutoLoadSave"].Value == true and OrionLib.Flags["AutoCollectWork"].Value == true then
         game:GetService("ReplicatedStorage").Events.CollectWork:InvokeServer()
     end
 end)
