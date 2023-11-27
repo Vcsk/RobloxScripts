836c2dad6d48168ece18ac33ab5e5436ce84f098
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-11-27 22:00:39 +0800

    Update main.lua

--- Games/RarityFactoryTycoon/main.lua
+++ Games/RarityFactoryTycoon/main.lua
@@ -1,11 +1,13 @@
 getgenv().settings = {
 	AutoCollectOrbs = false,
+	AutoCollectDiamonds = false,
 	AutoCollectTreeOrbs = false,
 	AutoSell = false,
 	AutoBuy = false,
 	AutoRebirth = false,
 	AutoClaimGift = false,
-	AutoObby = false
+	AutoObby = false,
+	AutoLoadSave = false,
 }
 
 local player = game.Players.LocalPlayer
@@ -18,7 +20,7 @@ for _, v in pairs(game.Workspace.Tycoons:GetDescendants()) do
 	end
 end
 
-function AutoCollectOrbs()
+local function AutoCollectOrbs()
     for _, v in pairs(pTycoon.Drops:GetDescendants()) do
         if v.ClassName == "Part" then 
             v.CFrame = player.Character.Pickup.CFrame
@@ -26,7 +28,15 @@ function AutoCollectOrbs()
     end
 end
 
-function AutoCollectTreeOrbs()
+local function AutoCollectDiamonds()
+    for _, v in pairs(pTycoon.Drops:GetDescendants()) do
+        if v.Name == "DiamondSmall" or v.Name == "DiamondMedium" then 
+            v.CFrame = player.Character.Pickup.CFrame
+        end
+    end
+end
+
+local function AutoCollectTreeOrbs()
     for _, v in pairs(pTycoon.RebirthTree.Drops:GetDescendants()) do
         if v.ClassName == "Part" then 
             v.CFrame = player.Character.Pickup.CFrame
@@ -34,7 +44,7 @@ function AutoCollectTreeOrbs()
     end
 end
 
-function AutoBuy()
+local function AutoBuy()
     for _, v in pairs(pTycoon.Buttons:GetDescendants()) do
         if v.ClassName == "TouchTransmitter" then
             firetouchinterest(v.Parent, player.Character.HumanoidRootPart, 0)
@@ -43,7 +53,7 @@ function AutoBuy()
     end
 end
 
-function AutoSell()
+local function AutoSell()
     for _, v in pairs(pTycoon["Orb Processor"]:GetDescendants()) do
         if v.ClassName == "TouchTransmitter" then
             firetouchinterest(v.Parent, player.Character.HumanoidRootPart, 0)
@@ -52,7 +62,7 @@ function AutoSell()
     end
 end
 
-function AutoRebirth()
+local function AutoRebirth()
     for _, v in pairs(pTycoon.Rebirth:GetDescendants()) do
         if v.ClassName == "TouchTransmitter" then
             firetouchinterest(v.Parent, player.Character.HumanoidRootPart, 0)
@@ -61,7 +71,7 @@ function AutoRebirth()
     end
 end
 
-function AutoClaimGift()
+local function AutoClaimGift()
     game:GetService("ReplicatedStorage").Remotes.ClaimGift:FireServer(1)
     game:GetService("ReplicatedStorage").Remotes.ClaimGift:FireServer(2)
     game:GetService("ReplicatedStorage").Remotes.ClaimGift:FireServer(3)
@@ -77,25 +87,65 @@ function AutoClaimGift()
 end
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/UI-Library/main/Source/Orion.lua"))()
-local window = library:MakeWindow({Name = "Rarity Factory Tycoon", HidePremium = false, SaveConfig = true, ConfigFolder = "VX_RFT", IntroEnabled = false})
+local window = library:MakeWindow({Name = "Rarity Factory Tycoon", HidePremium = false, SaveConfig = true, ConfigFolder = "VX_RFT", IntroEnabled = true, IntroText = "Made by vcsk0"})
 
+local homeTab = window:MakeTab({
+    Name = "Home",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
 local mainTab = window:MakeTab({
 	Name = "Main",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+local settingsTab = window:MakeTab({
+    Name = "Settings",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+homeTab:AddParagraph("UPDATES","UPDATE 1: Added Auto Collect Orbs, Added Auto Sell, Added Auto Buy, Added Auto Rebirth, Added Auto Obby\nUPDATE 2: Added Auto Collect Tree Orbs\nUPDATE 3: Added Auto Collect Diamonds")
+
+homeTab:AddButton({
+    Name = "Discord Invite",
+    Callback = function()
+              setclipboard("https://discord.com/invite/cNBNvSufYw")
+              OrionLib:MakeNotification({
+                  Name = "Notification",
+                  Content = "Copied to your clipboard!",
+                  Image = "rbxassetid://4483345998",
+                  Time = 5
+              })
+      end
+})
 
 mainTab:AddToggle({
     Name = "Auto Collect Orbs",
     Default = false,
+    Save = true,
+	Flag = "AutoCollectOrbs",
     Callback = function(toggle)
         getgenv().settings.AutoCollectOrbs = toggle
     end
 })
 
 mainTab:AddToggle({
+    Name = "Auto Collect Diamonds",
+    Default = false,
+	Save = true,
+	Flag = "AutoCollectDiamonds",
+    Callback = function(toggle)
+        getgenv().settings.AutoCollectDiamonds = toggle
+    end
+})
+
+
+mainTab:AddToggle({
     Name = "Auto Collect Tree Orbs",
     Default = false,
+	Save = true,
+	Flag = "AutoCollectTreeOrbs",
     Callback = function(toggle)
         getgenv().settings.AutoCollectTreeOrbs = toggle
     end
@@ -104,6 +154,8 @@ mainTab:AddToggle({
 mainTab:AddToggle({
     Name = "Auto Sell",
     Default = false,
+	Save = true,
+	Flag = "AutoSell",
     Callback = function(toggle)
         getgenv().settings.AutoSell = toggle
     end
@@ -112,6 +164,8 @@ mainTab:AddToggle({
 mainTab:AddToggle({
     Name = "Auto Buy",
     Default = false,
+	Save = true,
+	Flag = "AutoBuy",
     Callback = function(toggle)
         getgenv().settings.AutoBuy = toggle
     end
@@ -120,6 +174,8 @@ mainTab:AddToggle({
 mainTab:AddToggle({
     Name = "Auto Rebirth",
     Default = false,
+	Save = true,
+	Flag = "AutoRebirth",
     Callback = function(toggle)
         getgenv().settings.AutoRebirth = toggle
     end
@@ -128,6 +184,8 @@ mainTab:AddToggle({
 mainTab:AddToggle({
     Name = "Auto Claim Gift",
     Default = false,
+	Save = true,
+	Flag = "AutoClaimGift",
     Callback = function(toggle)
         getgenv().settings.AutoClaimGift = toggle
     end
@@ -163,11 +221,27 @@ mainTab:AddToggle({
     end
 })
 
+settingsTab:AddToggle({
+    Name = "Auto Load Saved Configs",
+    Default = false,
+	Save = true,
+	Flag = "AutoLoadSave",
+    Callback = function(toggle)
+        getgenv().settings.AutoLoadSave = toggle
+    end
+})
+
 --// Main Codes
 
 game:GetService("RunService").Heartbeat:Connect(function()
 	if getgenv().settings.AutoCollectOrbs == true then
 	    AutoCollectOrbs()
+	end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+	if getgenv().settings.AutoCollectDiamonds == true then
+	    AutoCollectDiamonds()
 	end
 end)
 
@@ -200,5 +274,51 @@ game:GetService("RunService").Heartbeat:Connect(function()
 	    AutoClaimGift()
 	end
 end)
+
+--// Saves
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if OrionLib.Flags["AutoLoadSave"].Value == true and OrionLib.Flags["AutoCollectOrbs"].Value == true then
+	    AutoCollectOrbs()
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if OrionLib.Flags["AutoLoadSave"].Value == true and OrionLib.Flags["AutoCollectDiamonds"].Value == true then
+        AutoCollectDiamonds()
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if OrionLib.Flags["AutoLoadSave"].Value == true and OrionLib.Flags["AutoCollectTreeOrbs"].Value == true then
+        AutoCollectTreeOrbs()
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if OrionLib.Flags["AutoLoadSave"].Value == true and OrionLib.Flags["AutoSell"].Value == true then
+        AutoSell()
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if OrionLib.Flags["AutoLoadSave"].Value == true and OrionLib.Flags["AutoBuy"].Value == true then
+        AutoBuy()
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if OrionLib.Flags["AutoLoadSave"].Value == true and OrionLib.Flags["AutoRebirth"].Value == true then
+        AutoRebirth()
+    end
+end)
+
+game:GetService("RunService").Heartbeat:Connect(function()
+    if OrionLib.Flags["AutoLoadSave"].Value == true and OrionLib.Flags["AutoClaimGift"].Value == true then
+        AutoClaimGift()
+    end
+end)
+
+--// end
 
 library:Init()
