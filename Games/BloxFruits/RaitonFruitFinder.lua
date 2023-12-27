637ed00043d983410520150ae380e191c6d4fd43
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-12-28 07:08:58 +0800

    Create RaitonFruitFinder.lua

---
+++ Games/BloxFruits/RaitonFruitFinder.lua
@@ -0,0 +1,5 @@
+ServerHopTimer = 10
+TeleportSafe = true
+Team = "Marine"
+Webhook = "Your Webhook Here.."
+_G.Version="Fruit Farm"loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/RaitoHub/main/Script"))()
