7ce2811ce996110a711c57beb7585016fc21c1d2
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-12-28 07:12:45 +0800

    Update RaitonFruitFinder.lua

--- Games/BloxFruits/RaitonFruitFinder.lua
+++ Games/BloxFruits/RaitonFruitFinder.lua
@@ -1,5 +1,6 @@
-ServerHopTimer = 10
-TeleportSafe = true
-Team = "Marine"
-Webhook = "Your Webhook Here.."
-_G.Version="Fruit Farm"loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/RaitoHub/main/Script"))()
+ServerHopTimer = 10;
+TeleportSafe = true;
+Team = "Marine";
+Webhook = "Your webhooks here...";
+_G.Version="Fruit Farm";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/RaitoHub/main/Script"))();
